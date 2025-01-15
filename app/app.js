c22e7c8ceb0ca244153c82fed7301e489b5d7c4c
Paul Antezana <PAntezana@grupovalor.com>
2025-01-15 18:34:48 -0500

    new updates

--- app/app.js
+++ app/app.js
@@ -1,52 +1,52 @@
-import './scss/app.scss';
-import './prism/prism';
+// import './scss/app.scss';
+// import './prism/prism';
 
-import * as all from './script/colorScheme';
+// import * as all from './script/colorScheme';
 
-// Primary Menu
-SnMenu({
-    menuId: "SiteMenu",
-    toggleButtonID: "SiteMenu-toggle",
-    toggleClass: "SiteMenu-is-show",
-    contextId: "Site",
-    parentClose: true,
-    menuCloseID: "SiteMenu-wrapper",
-    iconClassDown: 'fas fa-chevron-down',
-    iconClassUp: 'fas fa-chevron-up',
-});
+// // Primary Menu
+// SnMenu({
+//     menuId: "SiteMenu",
+//     toggleButtonID: "SiteMenu-toggle",
+//     toggleClass: "SiteMenu-is-show",
+//     contextId: "Site",
+//     parentClose: true,
+//     menuCloseID: "SiteMenu-wrapper",
+//     iconClassDown: 'fas fa-chevron-down',
+//     iconClassUp: 'fas fa-chevron-up',
+// });
 
-// Aside menu
-SnMenu({
-    menuId: 'AsideMenu',
-    toggleButtonID: 'AsideMenu-toggle',
-    toggleClass: 'AsideMenu-is-show',
-    menuCloseID: 'AsideMenu-close',
-});
+// // Aside menu
+// SnMenu({
+//     menuId: 'AsideMenu',
+//     toggleButtonID: 'AsideMenu-toggle',
+//     toggleClass: 'AsideMenu-is-show',
+//     menuCloseID: 'AsideMenu-close',
+// });
 
-let codeBox = document.querySelectorAll('.CodeBox');
-if (codeBox) {
-    codeBox.forEach(item => {
-        let codeBoxShow = item.querySelector('.CodeBox-show');
-        let codeBoxCode = item.querySelector('.CodeBox-code');
-        let codeBoxCopy = item.querySelector('.CodeBox-copy');
-        if (codeBoxShow && codeBoxCode) {
-            codeBoxShow.addEventListener('click', () => {
-                codeBoxCode.classList.toggle('is-expand');
-            });
+// let codeBox = document.querySelectorAll('.CodeBox');
+// if (codeBox) {
+//     codeBox.forEach(item => {
+//         let codeBoxShow = item.querySelector('.CodeBox-show');
+//         let codeBoxCode = item.querySelector('.CodeBox-code');
+//         let codeBoxCopy = item.querySelector('.CodeBox-copy');
+//         if (codeBoxShow && codeBoxCode) {
+//             codeBoxShow.addEventListener('click', () => {
+//                 codeBoxCode.classList.toggle('is-expand');
+//             });
 
-            if (codeBoxCopy) {
-                codeBoxCopy.addEventListener('click', () => {
-                    let range = document.createRange();
-                    range.selectNode(codeBoxCode);
-                    window.getSelection().removeAllRanges();
-                    window.getSelection().addRange(range);
-                    document.execCommand("copy");
-                    window.getSelection().removeAllRanges();
+//             if (codeBoxCopy) {
+//                 codeBoxCopy.addEventListener('click', () => {
+//                     let range = document.createRange();
+//                     range.selectNode(codeBoxCode);
+//                     window.getSelection().removeAllRanges();
+//                     window.getSelection().addRange(range);
+//                     document.execCommand("copy");
+//                     window.getSelection().removeAllRanges();
 
-                    SnMessage.success({ content: 'copy success' });
-                });
-            }
-        }
+//                     SnMessage.success({ content: 'copy success' });
+//                 });
+//             }
+//         }
 
-    });
-}
+//     });
+// }
